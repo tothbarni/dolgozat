98563cf0d16f164368ce813eb74bfa38538e7202
Barni <tothbarni.06@gmail.com>
2024-11-07 08:28:05 +0100

    masodik

--- feladatok.py
+++ feladatok.py
@@ -1,5 +1,13 @@
+import random
 def feladat1():
     szam:int = int(input("Kérek egy páros számot : "))
     while (szam % 2 != 0):
         szam:int = int(input("Ez nem páros! PÁROS számot kérek! "))
 
+def feladat2():
+    db = 0
+    for i in range(0, 13, 1):
+        szam:int = int(random.random()* 141) + 10
+        if (szam % 3 == 0):
+            db += 1
+    return db
--- main.py
+++ main.py
@@ -4,3 +4,5 @@ print("1. feladat")
 feladatok.feladat1()
 
 print("2. feladat")
+db = feladatok.feladat2()
+print(f"A számok között {db}db 3-mal osztható szám van!")
